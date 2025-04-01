b386ff82adfbe45227cfd0cf22bae4fe2a30ff56
Shapin199 <mdtanvir054321@gmail.com>
2025-04-02 03:11:44 +0600

    loss

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', ejs);
+
+app.use((req,res,next)=>{
+    console.log(req.url);
+    console.log("this in middleware");
+    res.send('Hello World from middleware');
+    return next();
+}
+)
+
+
 app.get('/',(req,res)=>{
     res.send ('Hello World');
 })
