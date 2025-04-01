08f4cf4e33ec8d13a2ae81085aa6ba1ec8336654
Shapin199 <mdtanvir054321@gmail.com>
2025-04-02 01:42:47 +0600

    create server

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const catMe = require('cat-me');
-   
-console.log(catMe());
+const http = require('http'); 
+
+const server = http .createServer((req,res) =>{
+    console.log(req.url);
+    // res.end('Hello World');
+    if(req.url === '/'){
+        res.end('Welcome to our home page');
+    }
+    if(req.url === '/about'){
+        res.end('Here is our short history');
+    }  
+})
+
+server.listen(3000);
