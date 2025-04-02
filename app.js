e8590f590c3b0798afd0b1afa860d03b9cbefe66
Shapin199 <mdtanvir054321@gmail.com>
2025-04-03 03:35:31 +0600

    add form and css and js

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.set('view engine','ejs');
 
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use((req,res,next)=>{
     console.log("this is middleware");
     // res.send("this middleware is used to log the request and response objects");
     return next();
 })
-app.get('/',(req,res)=>{
+app.get('/',(req,res,next)=>{
+    const a=5;
+    const b=10;
+    console.log(a+b);
+
+    next();
+},(req,res)=>{
     res.render('index');
 })
 app.get('/about',(req,res)=>{
     res.send ('About Page');
+})
+
+app.post('/get-form-data',(req,res)=>{
+    console.log(req.body);
+    res.send('Form data received');
 })
 
 app.listen(3000)
